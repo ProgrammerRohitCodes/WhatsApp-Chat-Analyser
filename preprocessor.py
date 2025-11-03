086d546228bde931b7ce4c925452394cab633321
Rohit Nath <rohitnath49195@gmail.com>
2025-11-03 10:28:08 +0530

    Create preprocessor.py

---
+++ preprocessor.py
@@ -0,0 +1,56 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APMapm]{2}\s-\s'
+
+    # first try 12-hour format with AM/PM; fallback to 24-hour if it fails
+    try:
+        messages = re.split(pattern, data)[1:]
+        dates = re.findall(pattern, data)
+        df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %I:%M %p - ')
+    except ValueError:
+        # fallback (some exports don’t have AM/PM)
+        pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+        messages = re.split(pattern, data)[1:]
+        dates = re.findall(pattern, data)
+        df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users, messages = [], []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    # make 12-hour period labels with AM/PM
+    period = []
+    for hour in df['hour']:
+        start = pd.Timestamp(hour=hour, minute=0).strftime("%I%p")
+        end_hour = (hour + 1) % 24
+        end = pd.Timestamp(hour=end_hour, minute=0).strftime("%I%p")
+        period.append(f"{start}-{end}")
+
+    df['period'] = period
+
+    return df
